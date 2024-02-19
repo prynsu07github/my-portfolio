340e762830ffd19e568dd9cb17c33543ff5cc2c4
prynsu <prynsu879@gmail.com>
2024-02-19 14:49:29 +0530

    send mail functionality

--- src/Pages/contact.jsx
+++ src/Pages/contact.jsx
@@ -1,5 +1,25 @@
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser'
 import PageHeader from "../components/PageHeader";
 const Contact = () => {
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+    emailjs
+      .sendForm('service_5v1gdkb', 'template_fdk0nmg', form.current , {
+        publicKey: 'j2X7GbLqQichs3QhH',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div
       className="min-h-screen h-auto w-full bg-#fafafa relative flex justify-center items-center py-16 sm:py-32"
@@ -7,12 +27,12 @@ const Contact = () => {
     >
       <div className=" w-11/12 lg:w-9/12 flex justify-center items-center flex-col">
       <PageHeader heading="contact" subheading="Feel free to Contact me by submitting the form below and I will get back to you as soon as possible" />
-      <form className="bg-[#fff] flex flex-col p-5 sm:p-16 shadow-lg shadow-gray-400 rounded-lg w-full sm:w-8/12 mt-16 sm:mt-32 ">
+      <form ref={form} onSubmit={sendEmail} className="bg-[#fff] flex flex-col p-5 sm:p-16 shadow-lg shadow-gray-400 rounded-lg w-full sm:w-8/12 mt-16 sm:mt-32 ">
         <label htmlFor="name">Name</label>
         <input
           required
           type="text"
-          id="name"
+          name="user_name"
           className="text-[#333] bg-gray-100 p-5 rounded-lg outline-none border-none mb-5"
           placeholder="Enter your name"
         />
@@ -20,7 +40,7 @@ const Contact = () => {
         <input
           required
           type="text"
-          id="email"
+          name="user_email"
           className="text-[#333] bg-gray-100 p-5 rounded-lg outline-none border-none mb-5"
           placeholder="Enter your email"
         />
@@ -28,12 +48,12 @@ const Contact = () => {
         <textarea
           required
           type="text"
-          id="msg"
+          name="message"
           className="text-[#333] bg-gray-100 p-5 rounded-lg outline-none border-none mb-5"
           rows="10"
           placeholder="Enter your message"
         />
-        <button className="bg-purple-600 px-8 py-3 font-bold text-white text-lg sm:text-xl rounded-lg hover:-translate-y-1 transition duration-150 ease-in-out shadow-md shadow-purple-400 mt-5">
+        <button type="submit" className="bg-purple-600 px-8 py-3 font-bold text-white text-lg sm:text-xl rounded-lg hover:-translate-y-1 transition duration-150 ease-in-out shadow-md shadow-purple-400 mt-5">
           Submit
         </button>
       </form>
